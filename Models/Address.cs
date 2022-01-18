4425409a8873b7e87e8ef63d08d32e7a20bce4af
fagner02 <fagnerrc12@gmail.com>
2022-01-18 16:28:24 -0300

    Create Models

---
+++ Models/Address.cs
@@ -0,0 +1,9 @@
+namespace LogInApi.Models {
+    public class Address {
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ Models/Colabarator.cs
@@ -0,0 +1,9 @@
+namespace LogInApi.Models {
+    public class Colabarator {
+        public string Cpf { get; set; }
+        public string FullName { get; set; }
+        public string Sex { get; set; }
+        public string Phone { get; set; }
+        public Address Address { get; set; }
+    }
+}
