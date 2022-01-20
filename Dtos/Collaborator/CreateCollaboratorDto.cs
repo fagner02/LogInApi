83ea42dc01f98272833e637315f59ceefd680c60
Fagner Ferreira <84201632+fagner02@users.noreply.github.com>
2022-01-19 23:08:58 -0300

    Add Create Collaborator Dto

--- Dtos/Collaborator/CreateCollaboratorDto.cs
+++ Dtos/Collaborator/CreateCollaboratorDto.cs
@@ -1,10 +1,11 @@
 using System;
-
 namespace LogInApi.Dtos {
-    public class UpdateCollaboratorDto {
+    public class CreateCollaboratorDto {
+        public string Cpf { get; set; }
         public string FullName { get; set; }
         public string Sex { get; set; }
         public string Phone { get; set; }
-        public Guid AddressId { get; set; }
+        public Guid? AddressId { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
